4b011b9fed3cf4279e5b95368dca82f85a6c7061
damian16043 <damianroundtree96@gmail.com>
2019-08-13 00:42:29 -0400

    set up grids and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Grid} from '@material-ui/core'
+import youtube from './api/youtube'
+import {SearchBar, VideoDetail} from './components'
 
 class App extends React.Component{
     render(){
         return(
-            <h1>Clone Tube</h1>
+            <Grid justfy= 'center'container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <SearchBar/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* VIDEO LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
