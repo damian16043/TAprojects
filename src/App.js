0796798259feb73065cf0a61db976ab045c7fcd1
damian16043 <damianroundtree96@gmail.com>
2019-08-14 00:28:34 -0400

    added state to handle video data

--- src/App.js
+++ src/App.js
@@ -4,16 +4,34 @@ import youtube from './api/youtube'
 import {SearchBar, VideoDetail} from './components'
 
 class App extends React.Component{
+    state = {
+        video: [],
+        selectedVideo: null,
+    };
+
+    handleSubmit = async ({searchTerm}) => {
+        const response = await youtube.get('search', { params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyAFKZVEF6efMxZ7CkLjar5cgbigBuhwphc',
+            q: searchTerm,
+        }});
+
+        this.setState({video: response.data.items, selectedVideo: response.data.items[0]});
+        
+    }
+    
     render(){
+        const {selectedVideo} =  this.state;
         return(
-            <Grid justfy= 'center'container spacing={16}>
+            <Grid justfy= 'center'container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
-                        <Grid item xs={12}>
-                            <SearchBar/>
+                    <Grid container spacing={10}>
+                        <Grid item xs={10}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs={8}>
-                            <VideoDetail/>
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
                             {/* VIDEO LIST */}
